352661de527930da5a081b3a0f4bfed0d38adc17
agorbac <agorbaceva874@gmail.com>
2025-11-26 23:33:51 +0300

    test2

--- ex1/task1.s
+++
@@ -1,7 +0,0 @@
-.SECT .TEXT
-    MOV    AX, (x)
-    SUB    AX, (y)
-.SECT .DATA
-x:  .WORD   3
-y:  .WORD   2
-.SECT .BSS
--- ex1/task2.s
+++
@@ -1,7 +0,0 @@
-.SECT .TEXT
-    MOV    AX, (x)
-    SUB    AX, (y)
-.SECT .DATA
-x:  .WORD   5
-y:  .WORD   3
-.SECT .BSS
